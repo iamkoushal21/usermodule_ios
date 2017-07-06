bfc732133bec7113e4237196fbceb853ca81ccc7
7rcksh@gmail.com <7rcksh@gmail.com>
2017-07-06 16:27:48 +0530

    refactored the code

--- UserModule/ForgotPasswordViewController.swift
+++ UserModule/ForgotPasswordViewController.swift
@@ -7,13 +7,32 @@
 //
 
 import UIKit
+import MessageUI
 
-class ForgotPasswordViewController: UIViewController {
+class ForgotPasswordViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
+    @IBOutlet weak var emailIdTextField: UITextField!
+    
+    var emailId: String {
+        return emailIdTextField.text ?? ""
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func sendNewPasswordButtonTapped(_ sender: Any) {
+        if emailId.isEmpty {
+            showAlertViewController(alert: .emailNotValid)
+        } else {
+                let mailComposer = MFMailComposeViewController()
+                mailComposer.mailComposeDelegate = self
+                mailComposer.setToRecipients(["7rcksh@gmail.com"])
+                mailComposer.setSubject("New Password")
+                mailComposer.setMessageBody("Set Password", isHTML: false)
+                present(mailComposer, animated: true, completion: nil)
+            
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
